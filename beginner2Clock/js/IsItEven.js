2a2df1002a2e2e6b9ec267a1eeb033fcc15cf7a3
Finesse Carter <shop.scrxvms@gmail.com>
2020-03-29 20:21:21 -0400

    new codewar

---
+++ beginner2Clock/js/IsItEven.js
@@ -0,0 +1,30 @@
+/*In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats are considered UNeven for this kata.*/
+
+// my code
+function testEven(n) {
+    if(n%2===0){
+    return true 
+    }else{
+    return false
+    }
+   }
+/////////////////////////////
+
+
+   function testEven(n) {
+    return n%2===0;
+}
+
+
+
+function testEven(n){
+    return !(n % 2);
+  }
