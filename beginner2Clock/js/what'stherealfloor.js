319de594d1e10255ee67ee983aa9f1056202392f
Finesse Carter <shop.scrxvms@gmail.com>
2020-03-30 11:37:33 -0400

    new codewar

---
+++ beginner2Clock/js/what'stherealfloor.js
@@ -0,0 +1,25 @@
+/* Task Overview
+Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor ('cause of superstition).
+
+Write a function that given an American floor (passed as an integer) returns the real floor.
+Moreover, your function should work for basement floors too: just return the same value as the passed one.
+
+Usage Examples
+getRealFloor(1) == 0 
+getRealFloor(0) == 0 // Special case to please Europeans
+getRealFloor(5) == 4
+getRealFloor(15) == 13
+getRealFloor(-3) == -3*/
+
+// My Solution
+function getRealFloor(n) {
+    if(n>=13){
+    return n - 2 
+    }else if(n==0){
+    return 0
+    }else if(n<=0){
+    return n
+    }else{
+    return n-1
+    }
+    }
