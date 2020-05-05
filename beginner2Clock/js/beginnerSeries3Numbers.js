7c8cceb37ace7601ac99cc4768c5c24b5a26fd36
Finesse Carter <shop.scrxvms@gmail.com>
2020-05-05 19:41:37 -0400

    new codeware

---
+++ beginner2Clock/js/beginnerSeries3Numbers.js
@@ -0,0 +1,28 @@
+/* Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples
+GetSum(1, 0) == 1   // 1 + 0 = 1
+GetSum(1, 2) == 3   // 1 + 2 = 3
+GetSum(0, 1) == 1   // 0 + 1 = 1
+GetSum(1, 1) == 1   // 1 Since both are same
+GetSum(-1, 0) == -1 // -1 + 0 = -1
+GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+*/
+
+function getSum( a,b ){
+    let results =0; 
+    if(a<b){
+    for(let i = a;i<=b;i++){
+    results+=i
+    }
+    }else if(b<a){
+    for(let i = b;i<=a;i++){
+    results+=i
+    }
+    }else if(b===a){
+    results = a;
+    }
+    return results
+    }
