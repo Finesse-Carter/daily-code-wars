ff6faea40322fb18c0d922da5c1d45f11bc8235a
Finesse Carter <shop.scrxvms@gmail.com>
2020-05-10 21:17:08 -0400

    new codewar

--- beginner2Clock/js/Polishalphabet.js
+++ beginner2Clock/js/Polishalphabet.js
@@ -40,3 +40,20 @@ function correctPolishLetters (string) {
     var dict = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
     return string.replace(/[ąćęłńóśźż]/g, match => dict[match]);
   }
+
+
+  var polishLetters = {
+    "ą": "a",
+    "ć": "c",
+    "ę": "e",
+    "ł": "l",
+    "ń": "n",
+    "ó": "o",
+    "ś": "s",
+    "ź": "z",
+    "ż": "z",
+};
+
+function correctPolishLetters (string) {
+  return string.split('').map((c) => polishLetters[c] || c).join("");
+}
