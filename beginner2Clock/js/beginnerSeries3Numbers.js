15d4c069f36345ab4809d0886f27f1abda0f5049
Finesse Carter <shop.scrxvms@gmail.com>
2020-05-25 22:48:10 -0400

    new codewar

--- beginner2Clock/js/beginnerSeries3Numbers.js
+++ beginner2Clock/js/beginnerSeries3Numbers.js
@@ -1,4 +1,6 @@
-/* Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+/* Given two integers a and b, which can be positive or negative,
+ find the sum of all the numbers between including them too and return it. 
+ If the two numbers are equal return a or b.
 
 Note: a and b are not ordered!
 
