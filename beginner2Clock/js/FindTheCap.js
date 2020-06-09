f422492eaabcb71c25df679934d2e7968463744f
Finesse Carter <shop.scrxvms@gmail.com>
2020-06-09 13:31:36 -0400

    new codewar

---
+++ beginner2Clock/js/FindTheCap.js
@@ -0,0 +1,16 @@
+/**Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] ); */
+
+var capitals = function (word) {
+    // Write your code heref
+    let cap = []
+    for(let i = 0; i<word.length;i++){
+    if(word[i]===word[i].toUpperCase()){
+    cap.push(i)
+    }
+    }
+    return cap
+  };
