21bd293419124642cf69665247b1a7554110a91a
Finesse Carter <shop.scrxvms@gmail.com>
2020-06-15 16:25:18 -0400

    new codewar

---
+++ beginner2Clock/js/GreedisGood.js
@@ -0,0 +1,68 @@
+/**Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+
+ Three 1's => 1000 points
+ Three 6's =>  600 points
+ Three 5's =>  500 points
+ Three 4's =>  400 points
+ Three 3's =>  300 points
+ Three 2's =>  200 points
+ One   1   =>  100 points
+ One   5   =>   50 point
+A single die can only be counted once in each roll. For example, a "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+Example scoring
+
+ Throw       Score
+ ---------   ------------------
+ 5 1 3 4 1   50 + 2 * 100 = 250
+ 1 1 1 3 1   1000 + 100 = 1100
+ 2 4 4 5 4   400 + 50 = 450
+In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests. */
+
+
+
+function score( dice ) {
+    let dictionary = {}
+    let sum = 0
+    for(let i = 0; i < dice.length; i++){
+      if(dictionary[dice[i]]){
+        dictionary[dice[i]]++
+      }else{
+        dictionary[dice[i]] = 1;
+      }
+    }
+    for(const num in dictionary){
+      if(num !== '1' && num !== '5'){
+                    console.log(dictionary)
+          while(dictionary[num] - 3 >= 0){
+          dictionary[num] = dictionary[num] - 3
+          sum+= (Number(num) * 100); 
+        }
+      } else if (num == '1' && dictionary[num] >= 3){
+        while(dictionary[num] - 3 >= 0){
+          dictionary[num] = dictionary[num] - 3
+          sum+= 1000; 
+        }
+        while(dictionary[num] > 0){
+          sum+= 100; 
+          dictionary[num]--
+        }
+      } else if (num == '1' && dictionary[num] < 3){
+        while(dictionary[num] > 0){
+          sum+= 100; 
+          dictionary[num]--
+        }
+      } else if (num == '5' && dictionary['5'] < 3 ){
+        while(dictionary[num] > 0){
+          sum+= 50
+          dictionary[num]--
+        }
+      } else if (num == '5' && dictionary['5'] >= 3 ){
+        while(dictionary[num] - 3 >= 0){
+          dictionary[num] = dictionary[num] - 3
+          sum+= (Number(num) * 100); 
+        }
+      }
+    }  
+    return sum
+  }
