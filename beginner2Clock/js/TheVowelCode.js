53682f2af623820bf21342d99b9a5a2cbf9d6865
Finesse Carter <shop.scrxvms@gmail.com>
2020-06-17 23:58:58 -0400

    new codewar

--- beginner2Clock/js/TheVowelCode.js
+++ beginner2Clock/js/TheVowelCode.js
@@ -47,3 +47,15 @@ function encode(string) {
       return arr.join('')
     }
     
+
+    /////////////////////////////
+
+    // turn vowels into numbers
+function encode(string){
+    return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
+  }
+  
+  //turn numbers back into vowels
+  function decode(string){
+    return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
+  }
