2d3d27b8426a3b7e610b3623be3bb3e03872ef65
Finesse Carter <shop.scrxvms@gmail.com>
2020-07-02 12:31:44 -0400

    new codewar

--- beginner2Clock/js/AdjacentRepeatedwordsinastring.js
+++ beginner2Clock/js/AdjacentRepeatedwordsinastring.js
@@ -1,5 +1,6 @@
 /**Description:
-You know how sometimes you write the the same word twice in a sentence, but then don't notice that it happened? For example, you've been distracted for a second. Did you notice that *"the"* is doubled in the first sentence of this description?
+You know how sometimes you write the the same word twice in a sentence, but then don't notice that it happened? 
+For example, you've been distracted for a second. Did you notice that *"the"* is doubled in the first sentence of this description?
 
 As as aS you can see, it's not easy to spot those errors, especially if words differ in case, like *"as"* at the beginning of the sentence.
 
