b8db5d444aa4c40c3d0a3d8ab3cc052bd86ae66a
Finesse Carter <shop.scrxvms@gmail.com>
2020-07-26 16:12:56 -0400

    new codewar

--- beginner2Clock/js/Sum of differences in array.js
+++ beginner2Clock/js/Sum of differences in array.js
@@ -8,42 +8,11 @@ Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
 
 If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell). */
   
-  const sumOfDifferences = (arr) => {
-    let newArr = arr.sort(function(a, b) {
-      return a - b;
-    });
-    //console.log('here is newArr: ' + newArr);
-    let sortArr = [];
-    let change = false;
-    let first;
-    let second;
-    let sum = 0;
-  
-    if(arr.length < 2) {
-      return 0;
-    }
-  
-    //puts the array in reverse order
-    for(let i = newArr.length - 1; i > -1; i--) {
-      sortArr.push(newArr[i]);
-    }
-    //console.log('here is the sorted array: ' + sortArr);
-  
-    //get the sum by looping through the array
-    for(let j = 0; j < sortArr.length - 1; j++) {
-      if(change === false && j === 0) {
-        first = sortArr[j] - sortArr[j + 1];
-        change = !change;
-        continue;
-      }
-      if(change === true) {
-        second = sortArr[j] - sortArr[j + 1];
-        sum = sum + (first + second);
-        first = second;
-        continue;
-      }
-    }
-  
-    console.log('here is sum: ' + sum);
-    return sum;
-  }
+const sumOfDifferences = arr =>
+[...arr]
+  .sort((a, b) => b - a)
+  .reduce(
+    (total, number, index, array) =>
+      index + 1 < array.length ? total + number - array[index + 1] : total,
+    0
+  )
