2caa9c931d449d61d1380a85121101b42ef7cbe1
Finesse-Carter <shop.scrxvms@gmail.com>
2020-09-21 06:16:26 -0400

    new codeware

---
+++ beginner2Clock/js/String repeat.js
@@ -0,0 +1,10 @@
+// Write a function called repeat_str which repeats the given string src exactly count times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+const repeatStr = (n, s) => s.repeat(n);
