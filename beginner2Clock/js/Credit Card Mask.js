b66f3c0c2297c6432ebcb1097414634a8adc8c0b
Finesse-Carter <shop.scrxvms@gmail.com>
2020-09-24 19:01:27 -0400

    new code war

---
+++ beginner2Clock/js/Credit Card Mask.js
@@ -0,0 +1,21 @@
+/**Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples
+maskify("4556364607935616") == "############5616"
+maskify(     "64607935616") ==      "#######5616"
+maskify(               "1") ==                "1"
+maskify(                "") ==                 ""
+
+// "What was the name of your first pet?"
+maskify("Skippy")                                   == "##ippy"
+maskify("Nananananananananananananananana Batman!") == "####################################man!" */
+
+const maskify = cc => {
+    var arr=[]
+    for(let i = cc.length-1; i >= 0; i--){
+      arr.push(arr.length > 3 ? '#' : cc[i])
+    }
+      return arr.reverse().join('')
+    }
