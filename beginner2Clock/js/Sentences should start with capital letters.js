b625f0292c35fc8a24bdbcc579487071c7da6b6d
Finesse-Carter <shop.scrxvms@gmail.com>
2020-10-14 07:30:03 -0400

    war

---
+++ beginner2Clock/js/Sentences should start with capital letters.js
@@ -0,0 +1,11 @@
+/**In English, all words at the begining of a sentence should begin with a capital letter.
+
+You will be given a paragraph that does not use capital letters. Your job is to capitalise the first letter of the first word of each sentence.
+
+For example,
+
+input:
+
+"hello. my name is inigo montoya. you killed my father. prepare to die."
+
+output: */
