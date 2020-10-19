255bfca3346176e0d5212e7c26bf5c111fbd5c5f
Finesse-Carter <shop.scrxvms@gmail.com>
2020-10-18 22:00:58 -0400

    war

--- beginner2Clock/js/Are You Playing Banjo.js
+++ beginner2Clock/js/Are You Playing Banjo.js
@@ -2,7 +2,7 @@
 If your name starts with the letter "R" or lower case "r", you are playing banjo!
 
 The function takes a name as its only argument, and returns one of the following strings:
-
+10-18-20
 name + " plays banjo" 
 name + " does not play banjo"
 Names given are always valid strings. */
