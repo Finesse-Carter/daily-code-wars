17cd842227d8517c361a5e36116377a36ccd5c19
Finesse-Carter <shop.scrxvms@gmail.com>
2020-12-01 06:42:48 -0500

    code

--- beginner2Clock/js/testing.js
+++ beginner2Clock/js/testing.js
@@ -1,34 +1,15 @@
-// function findShort(s){
-//     let a = s.split(' ');
-//     let b = a.sort(function(a, b){return a.length - b.length});
-//     console.log(b[0].length);
+function consecutiveDucks(num) {
+    var a=[]
+    for(var i = 1; i<num; i++){
+    a.push(i)
 
-// }
-// findShort("bitcoin take over the world maybe who knows perhaps");
-// findShort("turns out random test cases are easier than writing out basic ones");
-
-function invert(array) {
-    //console.log(array);
-    let b = [];
-    let v = array.join(' ')
-    console.log(v);
-
-    // let a = array.map(b=>console.log(b));
-    v = ~v + 1; 
-    console.log(v);
-
- }
-
-invert([1,-2,3,-4,5])
-let g =5
-g = ~g + 1; 
-    console.log(g);
-
-// [0]
-//   [-1,2,-3,4,-5]
-//  []
-let x = [1,-2,3,-4,5]
-let v = x.join(' ')
-v.split(' ')
-console.log(typeof v);
-console.log(v);
+      console.log(a);
+    }
+  }
+  consecutiveDucks(10)
+  /*
+  p.num
+  r. t or f 
+  e.(57), true (16), false
+  p. if().  for()
+  */
