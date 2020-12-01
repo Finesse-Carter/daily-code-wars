62a8c8ef7e41e0d907693877fe182c8737b7127c
Finesse-Carter <shop.scrxvms@gmail.com>
2020-12-01 06:46:13 -0500

    code

---
+++ beginner2Clock/js/Consecutive Ducks.js
@@ -0,0 +1,6 @@
+/**
+ * Positive integers have so many gorgeous features. Some of them could be expressed as a sum of two or more consecutive positive numbers.
+
+Consider an Example :
+10 , could be expressed as a sum of 1 + 2 + 3 + 4.
+ */
