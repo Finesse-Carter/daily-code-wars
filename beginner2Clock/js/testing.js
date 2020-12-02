dc62b65ff52d8b93fd83d89f96ecae33ac15b7dd
Finesse-Carter <shop.scrxvms@gmail.com>
2020-12-02 05:50:43 -0500

    code

--- beginner2Clock/js/testing.js
+++ beginner2Clock/js/testing.js
@@ -1,15 +1,8 @@
-function consecutiveDucks(num) {
-    var a=[]
-    for(var i = 1; i<num; i++){
-    a.push(i)
-
-      console.log(a);
+function multiTable(number) {
+    for(var i = 0; i <= number; i++){
+      console.log(i); 
+  //     'i * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50'
     }
   }
-  consecutiveDucks(10)
-  /*
-  p.num
-  r. t or f 
-  e.(57), true (16), false
-  p. if().  for()
-  */
+  multiTable(5)
+//   https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce/train/javascript
