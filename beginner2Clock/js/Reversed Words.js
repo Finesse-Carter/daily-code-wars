e578d4d16fb5322538c5ec618d12bcbd9169bac3
Finesse-Carter <shop.scrxvms@gmail.com>
2020-12-28 06:40:59 -0500

    code

---
+++ beginner2Clock/js/Reversed Words.js
@@ -0,0 +1,8 @@
+/**Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory */
+
+const reverseWords = str => str.split(' ').reverse().join(' '); 
