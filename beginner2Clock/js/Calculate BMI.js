a637bd8bfc4ce91a02e7353920c5162bbb25c1f1
Finesse-Carter <shop.scrxvms@gmail.com>
2021-01-21 20:39:37 -0500

    war

--- beginner2Clock/js/Calculate BMI.js
+++ beginner2Clock/js/Calculate BMI.js
@@ -7,3 +7,16 @@ if bmi <= 25.0 return "Normal"
 if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese" */
+
+function bmi(weight, height) {
+    var a = weight/height/height;
+     if (a <= 18.5){ 
+       return ("Underweight")
+     }else if (a <= 25.0) {
+       return ("Normal")
+     }else if (a <= 30.0){ 
+       return ("Overweight")
+     }else {
+       return ("Obese")
+           }
+   }
