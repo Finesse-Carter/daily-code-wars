30d19aed106e95c48412f9b3803a3fa32ec8672c
Finesse-Carter <shop.scrxvms@gmail.com>
2021-02-22 19:23:39 -0500

    war

--- beginner2Clock/js/Beginner - Reduce but Grow.js
+++ beginner2Clock/js/Beginner - Reduce but Grow.js
@@ -1,3 +1,5 @@
 //Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+let grow = x => x.reduce((a,b)=>a*b); 
