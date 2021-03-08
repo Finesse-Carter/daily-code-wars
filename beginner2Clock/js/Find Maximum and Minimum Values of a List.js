89b5b2801c0462fe3cb55babf7997fb42a7bf21a
Finesse-Carter <shop.scrxvms@gmail.com>
2021-03-08 16:59:27 -0500

    war

---
+++ beginner2Clock/js/Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,18 @@
+/* Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+#Examples
+
+max([4,6,2,1,9,63,-134,566]) returns 566
+min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+max([5]) returns 5
+min([42, 54, 65, 87, 0]) returns 0 */
+
+const min = (list) => {
+    let a = list.sort((b,c)=>b-c)
+    return a[0];
+}
+
+const max = (list) => {
+    let a = list.sort((b,c)=>c-b)
+    return a[0];
+}
