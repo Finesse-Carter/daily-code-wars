5db78a1236a453a10fd88bbb232e1b25d91125d9
Finesse-Carter <shop.scrxvms@gmail.com>
2021-03-14 23:23:27 -0400

    ok

--- beginner2Clock/js/The most asked question on CodeWars.js
+++ beginner2Clock/js/The most asked question on CodeWars.js
@@ -6,6 +6,6 @@ Can someone explain
 You need to write a function detect that will check if a string starts with Can someone explain, case sensitive. Return true if so, false otherwise.
 
 Let's hope you don't write a solution that makes people ask that question at you!
- */
+ *///
 
 const detect = comment => comment.startsWith('Can someone explain')
