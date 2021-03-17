33d69a2ba923d8eabdbe1eca0eae1a13e4e6de1e
Finesse-Carter <shop.scrxvms@gmail.com>
2021-03-17 12:21:39 -0400

    war

--- beginner2Clock/js/Merge two sorted arrays into one.js
+++ beginner2Clock/js/Merge two sorted arrays into one.js
@@ -1,1 +1,30 @@
+/**You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
 
+You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+Examples
+arr1 = [1, 2, 3, 4, 5];
+arr2 = [6, 7, 8, 9, 10];
+mergeArrays(arr1, arr2);  // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+arr3 = [1, 3, 5, 7, 9];
+arr4 = [10, 8, 6, 4, 2];
+mergeArrays(arr3, arr4);  // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+arr5 = [1, 3, 5, 7, 9, 11, 12];
+arr6 = [1, 2, 3, 4, 5, 10, 12];
+mergeArrays(arr5, arr6);  // [1, 2, 3, 4, 5, 7, 9, 10, 11, 12] */
+
+const mergeArrays = (arr1, arr2) => {
+    let a = []
+    let c = []
+         a.push(...arr1)
+        a.push(...arr2)
+     let b = a.sort((x,y)=>x-y);
+     let set = new Set(b)
+     c.push(...set)
+     return c
+    }
+    
