b4df402f8a57dd92d1d211a46f5db4e4e5b4b967
Finesse-Carter <shop.scrxvms@gmail.com>
2021-03-22 23:22:10 -0400

    war

--- beginner2Clock/js/Transportation on vacation.js
+++ beginner2Clock/js/Transportation on vacation.js
@@ -5,3 +5,16 @@ You will need a rental car in order for you to get around in your vacation. The 
 Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
 Write a code that gives out the total amount for different days(d). */
+
+const rentalCarCost = d => {
+    var a = d * 40
+    if(d >= 7){
+      a-=50
+      return a 
+    }else if(d >= 3){
+      a-=20
+      return a 
+    }else {
+      return a 
+    }
+  }
