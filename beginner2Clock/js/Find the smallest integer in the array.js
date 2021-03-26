a48230c161ac4e2a0c57780efab73f01c0dc0596
Finesse-Carter <shop.scrxvms@gmail.com>
2021-03-26 19:40:00 -0400

    ware

--- beginner2Clock/js/Find the smallest integer in the array.js
+++ beginner2Clock/js/Find the smallest integer in the array.js
@@ -6,3 +6,9 @@ Given [34, 15, 88, 2] your solution will return 2
 Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty. */
 
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      var a = args.sort((c,b)=>c-b)
+   return a[0]  
+    }
+  }
