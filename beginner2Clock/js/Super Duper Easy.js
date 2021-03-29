c09ea58f0f683c221bfc7ef40f6e1523878b85f7
Finesse-Carter <shop.scrxvms@gmail.com>
2021-03-29 10:37:28 -0400

    war

--- beginner2Clock/js/Super Duper Easy.js
+++ beginner2Clock/js/Super Duper Easy.js
@@ -1,1 +1,3 @@
 /**Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error". */
+
+const problem = x => typeof x === 'string' ? 'Error' : (x*50)+6
