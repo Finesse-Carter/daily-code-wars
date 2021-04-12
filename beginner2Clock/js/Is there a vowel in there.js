bb4909556593a0ff202b744296ebfbd1a7964b9d
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-12 13:05:01 -0400

    war

--- beginner2Clock/js/Is there a vowel in there.js
+++ beginner2Clock/js/Is there a vowel in there.js
@@ -3,3 +3,18 @@
 If they are, change the array value to a string of that vowel.
 
 Return the resulting array. */
+
+const isVow = a => a.map(x=>{ if (x===97) {
+    return ('a');
+} else if(x===101) {
+   return('e');
+} else if(x===105) {
+    return('i');
+}else if(x===111) {
+    return('o');
+}else if(x===117) {
+    return('u');
+}else{
+    return(x);
+}
+})
