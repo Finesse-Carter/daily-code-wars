19d321353483c16717b7688e98b94f6753ec1044
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-15 14:56:52 -0400

    war

--- beginner2Clock/js/Sum of Multiples.js
+++ beginner2Clock/js/Sum of Multiples.js
@@ -5,3 +5,19 @@
 // n and m are natural numbers (positive integers)
 // m is excluded from the multiples
 
+function sumMul(n,m){
+    let a = []
+    let b
+    if(n<=0 || m<=0){
+      return "INVALID"
+    }
+     for (let i = n; i < m; i++) {
+       if (i%n===0) {
+         a.push(i)
+         
+       b = a.reduce((x,y)=>x+y); 
+     }
+   }
+     return b
+   }
+   
