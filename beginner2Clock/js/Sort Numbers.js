b37fa511b8132e473536ac807511cd0ab897e889
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-21 18:04:42 -0400

    const solution = nums => nums === null || nums === [] ? [] : nums.sort((a,b)=> a-b)

--- beginner2Clock/js/Sort Numbers.js
+++ beginner2Clock/js/Sort Numbers.js
@@ -4,3 +4,5 @@ For example:
 
 solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 solution(null); // should return [] */
+
+const solution = nums => nums === null || nums === [] ? [] : nums.sort((a,b)=> a-b)
