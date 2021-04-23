b73039b406aff0f98f2ae4fe3ebc17c9b3f91be6
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-22 21:18:04 -0400

    war

--- beginner2Clock/js/pick a set of first elements.js
+++ beginner2Clock/js/pick a set of first elements.js
@@ -10,3 +10,14 @@ first(arr, 2) //=> ['a', 'b']
 first(arr, 3) //=> ['a', 'b', 'c'];
 first(arr, 0) //=> [];
  */
+
+const first = (arr, n) =>{
+    if(n === 0 ){
+      return []
+    } else if(n > 1){
+      return arr.slice(0,n)
+    }else {
+    return arr.slice(0,1)  
+    }
+  }
+  
