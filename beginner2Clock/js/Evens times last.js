e1b765afcb70f75e7d62df976ce3611adc10b0f7
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-23 17:29:16 -0400

    const evenLast = numbers => numbers.length === 0 ? 0 :numbers.filter((x,d)=>d%2===0).reduce((y,z)=> y+z)*numbers.slice(-1)

--- beginner2Clock/js/Evens times last.js
+++ beginner2Clock/js/Evens times last.js
@@ -1,3 +1,5 @@
 /**Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
 
 If the sequence is empty, you should return 0. */
+
+const evenLast = numbers => numbers.length === 0 ? 0 :numbers.filter((x,d)=>d%2===0).reduce((y,z)=> y+z)*numbers.slice(-1)
