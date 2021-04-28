93db079268b90e913a3331f7d287424d5fa1ef07
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-27 21:50:09 -0400

    war

--- beginner2Clock/js/Remove duplicates from list.js
+++ beginner2Clock/js/Remove duplicates from list.js
@@ -1,3 +1,8 @@
 /**Define a function that removes duplicates from an array of numbers and returns it as a result.
 
 The order of the sequence has to stay the same. */
+
+const distinct = a => {
+    let set = new Set(a)
+   return [...set]
+ }
