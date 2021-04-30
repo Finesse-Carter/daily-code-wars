8c203e4db6298ef0f7367c2aa08f00cadc9bdc12
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-30 13:31:27 -0400

    const isPalindrome = (x) => x.toLowerCase() === x.split('').reverse('').join('').toLowerCase() ? true : false

--- beginner2Clock/js/Is it a palindrome.js
+++ beginner2Clock/js/Is it a palindrome.js
@@ -1,1 +1,2 @@
 /**Write function that checks if a given string (case insensitive) is a palindrome. */
+const isPalindrome = x => x.toLowerCase() === x.split('').reverse('').join('').toLowerCase() ? true : false 
