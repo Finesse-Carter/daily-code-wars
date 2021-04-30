5f6a05be9d1b320b06c9037ac7bfa5b380638e58
Finesse-Carter <shop.scrxvms@gmail.com>
2021-04-30 13:30:06 -0400

    Is it a palindrome?

--- beginner2Clock/js/Fix your code before the garden dies!.js
+++ beginner2Clock/js/Fix your code before the garden dies!.js
@@ -2,3 +2,7 @@
 
 Your task is to debug the code before your plants die! */
 
+const rainAmount = (mm) =>
+  mm < 40
+    ? `You need to give your plant ${40 - mm}mm of water`
+    : "Your plant has had more than enough water for today!";
