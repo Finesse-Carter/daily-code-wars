93fce7597d2dd27262c10ae693f1a3e2f8edff97
Finesse-Carter <shop.scrxvms@gmail.com>
2021-05-07 08:21:31 -0400

    war

--- beginner2Clock/js/Return the day.js
+++ beginner2Clock/js/Return the day.js
@@ -10,3 +10,28 @@
 Otherwise returns "Wrong, please enter a number between 1 and 7"
  */
 
+function whatday(num) {
+  switch (num) {
+    case 1:
+      return "Sunday";
+      break;
+    case 2:
+      return "Monday";
+      break;
+    case 3:
+      return "Tuesday";
+      break;
+    case 4:
+      return "Wednesday";
+    case 5:
+      return "Thursday";
+      break;
+    case 6:
+      return "Friday";
+      break;
+    case 7:
+      return "Saturday";
+    default:
+      return "Wrong, please enter a number between 1 and 7";
+  }
+}
